6b76bfbd095161b91a1d00973d146accbc329ea0
Jesse <jelloeater@gmail.com>
2019-01-17 00:18:55 -0500

    Added threading Need to test

--- UptimeParser.py
+++ UptimeParser.py
@@ -1,14 +1,15 @@
 import logging
-import multiprocessing
+import threading
+from multiprocessing.pool import ThreadPool
 
+from pysnmp.error import PySnmpError
 
-logging.basicConfig(filename="uptime.log",
-                    format="[%(asctime)s] [%(levelname)8s] --- %(message)s (%(filename)s:%(lineno)s)",
-                    level=logging.WARNING)
+logging.basicConfig(  # filename="uptime.log",
+    format="[%(asctime)s] [%(levelname)8s] --- %(message)s (%(filename)s:%(lineno)s)",
+    level=logging.DEBUG)
 
 import datetime
 from pysnmp.hlapi import *
-
 
 
 def get_device_list():
@@ -21,20 +22,19 @@ def get_device_list():
     return device_list
 
 
-class device:
+class device():
     name: str
     up_time: datetime.timedelta
 
-    def __init__(self,name_in):
+    def __init__(self, name_in):
         self.name = name_in
         self.up_time = None
 
-    @staticmethod
-    def update_uptime(name_in, snmp_comm_in='public', snmp_port_in=161):
+    def update_uptime(self, snmp_comm_in='public', snmp_port_in=161):
         errorIndication, errorStatus, errorIndex, varBinds = next(
             getCmd(SnmpEngine(),
                    CommunityData(snmp_comm_in, mpModel=0),
-                   UdpTransportTarget((name_in, snmp_port_in)),
+                   UdpTransportTarget((self.name, snmp_port_in)),
                    ContextData(),
                    ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysUpTime', 0)))
         )
@@ -51,9 +51,9 @@ class device:
         try:
             ticks = int(varBinds[0][1])
             seconds = ticks / 100
-            return datetime.timedelta(seconds=seconds)
+            self.up_time = datetime.timedelta(seconds=seconds)
         except IndexError:
-            return False
+            self.up_time = False
 
     def is_over_x_hours(self, over_hours=24):
         dt = datetime.timedelta(hours=over_hours)
@@ -63,26 +63,42 @@ class device:
             return False
 
 
+def update_device_obj_uptime(device_obj: device) -> device:
+    '''Takes in device object and updates it's uptime property'''
+    logging.debug(threading.current_thread())
+    try:
+        device_obj.update_uptime()
+    except PySnmpError:
+        logging.error("Bad Address: " + device_obj.name)
+    return device_obj
+
+
 def main():
     device_list = get_device_list()
 
+    # This takes a list of device objects (missing uptime), and then maps them to a threadpool
+    # This pool then gets executed and the results of the static helper function and passed into a list
 
-
-    for i in device_list:
-        # p = multiprocessing.Process()
-        # p.start()
-        i.up_time = i.update_uptime(i.name)
-
+    pool = ThreadPool()
+    results = pool.map(update_device_obj_uptime, device_list) # Takes function to work against, and itterable list to work on
+    pool.close()
+    pool.join()
 
     devices_over_time_limit = 0
-    for i in device_list:
+    for i in results:
         if i.up_time:
             if i.is_over_x_hours():
                 devices_over_time_limit = devices_over_time_limit + 1
         else:
             logging.error("Cannot get up time from: " + str(i.name))
-    print(devices_over_time_limit)
+
+    pool.terminate()
+    return devices_over_time_limit
 
 
 if __name__ == "__main__":
-    main()
+    logging.info("START")
+    x = main()
+    logging.info(x)
+    logging.info("STOP")
+    print(x)
