128b42002d3e737def794d7b2a6f43d06bcc9775
Jesse <jelloeater@gmail.com>
2019-01-20 11:31:18 -0500

    Need to review why execution is now taking so long after refactor

---
+++ tests/test_UptimeParser.py
@@ -0,0 +1,13 @@
+import logging
+import sys
+
+logging.basicConfig(format="[%(asctime)s] [%(levelname)8s] --- %(message)s (%(filename)s:%(lineno)s)",
+                    level=logging.INFO)
+
+sys.path.append('./UptimeParser')  # Needed to import module properly
+
+import UptimeParser.UptimeParserMain as UptimeParser
+class TestMainModule():
+    def test_create_XML_device_data(self):
+        test_data = {"some sensor":42,"more data":"text"}
+        logging.info("\n"+UptimeParser.main.generate_xml(test_data)+"\n")
